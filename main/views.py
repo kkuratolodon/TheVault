c826167bc985c26261888c72a70914678111cc8a
kkuratolodon <mif05123@gmail.com>
2023-09-12 00:12:58 +0700

    editing some item(s)

--- main/views.py
+++ main/views.py
@@ -6,6 +6,8 @@ from .models import Item
 # Create your views here.
 def show_main(request):
     items = Item.objects.all()
+    for item in items:
+        item.price = f"{item.price:,}".replace(',','.')
     context = {
         'items' : items,
         'app_name' : 'The Vault',
