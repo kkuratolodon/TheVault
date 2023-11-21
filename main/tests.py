5a879c552321a2656110d5e78dfb266bf653a4b6
kkuratolodon <mif05123@gmail.com>
2023-11-21 12:49:40 +0700

    tes

--- main/tests.py
+++ main/tests.py
@@ -7,10 +7,10 @@ from main.models import *
 class mainTest(TestCase):
     # testing dari tutorial 1
     def test_main_url_is_exist(self):
-        response = Client().get('/main/')
+        response = Client().get('/')
         self.assertEqual(response.status_code, 200)
 
     def test_main_using_main_template(self):
-        response = Client().get('/main/')
+        response = Client().get('')
         self.assertTemplateUsed(response, 'main.html')
     
